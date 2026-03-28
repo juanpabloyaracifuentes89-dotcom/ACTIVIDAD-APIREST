01115deb29ef34177ac6624b8daae51801175b91
JuanYara12 <juanpabloyaracifuentes@gmail.com>
2026-03-28 17:12:35 -0500

    conexión backend railway

--- frontend/src/services/axiosConfig.js
+++ frontend/src/services/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:4000/api',
+    baseURL: 'actividad-apirest-production.up.railway.app',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
